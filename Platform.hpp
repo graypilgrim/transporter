0392af5598bc4d6374dba1917d12bcf4652a7a4a
Lukasz Wlazly <lukwlazly@gmail.com>
2017-01-03 23:34:45 +0100

    first lighting experiments

--- Platform.hpp
+++ Platform.hpp
@@ -10,47 +10,47 @@ class Platform
 public:
 	Platform(GLfloat sideWidth) {
 		GLfloat tempVertices[] = {
-	        -0.5f * sideWidth, -height, -0.5f * sideWidth,  0.0f, 0.0f,
-	         0.5f * sideWidth, -height, -0.5f * sideWidth,  1.0f, 0.0f,
-	         0.5f * sideWidth,  height, -0.5f * sideWidth,  1.0f, 1.0f,
-	         0.5f * sideWidth,  height, -0.5f * sideWidth,  1.0f, 1.0f,
-	        -0.5f * sideWidth,  height, -0.5f * sideWidth,  0.0f, 1.0f,
-	        -0.5f * sideWidth, -height, -0.5f * sideWidth,  0.0f, 0.0f,
+	        -0.5f * sideWidth, -height, -0.5f * sideWidth, 0.0f,  0.0f, -1.0f,
+	         0.5f * sideWidth, -height, -0.5f * sideWidth, 0.0f,  0.0f, -1.0f,
+	         0.5f * sideWidth,  height, -0.5f * sideWidth, 0.0f,  0.0f, -1.0f,
+	         0.5f * sideWidth,  height, -0.5f * sideWidth, 0.0f,  0.0f, -1.0f,
+	        -0.5f * sideWidth,  height, -0.5f * sideWidth, 0.0f,  0.0f, -1.0f,
+	        -0.5f * sideWidth, -height, -0.5f * sideWidth, 0.0f,  0.0f, -1.0f,
 
-	        -0.5f * sideWidth, -height,  0.5f * sideWidth,  0.0f, 0.0f,
-	         0.5f * sideWidth, -height,  0.5f * sideWidth,  1.0f, 0.0f,
-	         0.5f * sideWidth,  height,  0.5f * sideWidth,  1.0f, 1.0f,
-	         0.5f * sideWidth,  height,  0.5f * sideWidth,  1.0f, 1.0f,
-	        -0.5f * sideWidth,  height,  0.5f * sideWidth,  0.0f, 1.0f,
-	        -0.5f * sideWidth, -height,  0.5f * sideWidth,  0.0f, 0.0f,
+	        -0.5f * sideWidth, -height,  0.5f * sideWidth, 0.0f,  0.0f,  1.0f,
+	         0.5f * sideWidth, -height,  0.5f * sideWidth, 0.0f,  0.0f,  1.0f,
+	         0.5f * sideWidth,  height,  0.5f * sideWidth, 0.0f,  0.0f,  1.0f,
+	         0.5f * sideWidth,  height,  0.5f * sideWidth, 0.0f,  0.0f,  1.0f,
+	        -0.5f * sideWidth,  height,  0.5f * sideWidth, 0.0f,  0.0f,  1.0f,
+	        -0.5f * sideWidth, -height,  0.5f * sideWidth, 0.0f,  0.0f,  1.0f,
 
-	        -0.5f * sideWidth,  height,  0.5f * sideWidth,  1.0f, 0.0f,
-	        -0.5f * sideWidth,  height, -0.5f * sideWidth,  1.0f, 1.0f,
-	        -0.5f * sideWidth, -height, -0.5f * sideWidth,  0.0f, 1.0f,
-	        -0.5f * sideWidth, -height, -0.5f * sideWidth,  0.0f, 1.0f,
-	        -0.5f * sideWidth, -height,  0.5f * sideWidth,  0.0f, 0.0f,
-	        -0.5f * sideWidth,  height,  0.5f * sideWidth,  1.0f, 0.0f,
+	        -0.5f * sideWidth,  height,  0.5f * sideWidth, -1.0f,  0.0f,  0.0f,
+	        -0.5f * sideWidth,  height, -0.5f * sideWidth, -1.0f,  0.0f,  0.0f,
+	        -0.5f * sideWidth, -height, -0.5f * sideWidth, -1.0f,  0.0f,  0.0f,
+	        -0.5f * sideWidth, -height, -0.5f * sideWidth, -1.0f,  0.0f,  0.0f,
+	        -0.5f * sideWidth, -height,  0.5f * sideWidth, -1.0f,  0.0f,  0.0f,
+	        -0.5f * sideWidth,  height,  0.5f * sideWidth, -1.0f,  0.0f,  0.0f,
 
-	         0.5f * sideWidth,  height,  0.5f * sideWidth,  1.0f, 0.0f,
-	         0.5f * sideWidth,  height, -0.5f * sideWidth,  1.0f, 1.0f,
-	         0.5f * sideWidth, -height, -0.5f * sideWidth,  0.0f, 1.0f,
-	         0.5f * sideWidth, -height, -0.5f * sideWidth,  0.0f, 1.0f,
-	         0.5f * sideWidth, -height,  0.5f * sideWidth,  0.0f, 0.0f,
-	         0.5f * sideWidth,  height,  0.5f * sideWidth,  1.0f, 0.0f,
+	         0.5f * sideWidth,  height,  0.5f * sideWidth, 1.0f,  0.0f,  0.0f,
+	         0.5f * sideWidth,  height, -0.5f * sideWidth, 1.0f,  0.0f,  0.0f,
+	         0.5f * sideWidth, -height, -0.5f * sideWidth, 1.0f,  0.0f,  0.0f,
+	         0.5f * sideWidth, -height, -0.5f * sideWidth, 1.0f,  0.0f,  0.0f,
+	         0.5f * sideWidth, -height,  0.5f * sideWidth, 1.0f,  0.0f,  0.0f,
+	         0.5f * sideWidth,  height,  0.5f * sideWidth, 1.0f,  0.0f,  0.0f,
 
-	        -0.5f * sideWidth, -height, -0.5f * sideWidth,  0.0f, 1.0f,
-	         0.5f * sideWidth, -height, -0.5f * sideWidth,  1.0f, 1.0f,
-	         0.5f * sideWidth, -height,  0.5f * sideWidth,  1.0f, 0.0f,
-	         0.5f * sideWidth, -height,  0.5f * sideWidth,  1.0f, 0.0f,
-	        -0.5f * sideWidth, -height,  0.5f * sideWidth,  0.0f, 0.0f,
-	        -0.5f * sideWidth, -height, -0.5f * sideWidth,  0.0f, 1.0f,
+	        -0.5f * sideWidth, -height, -0.5f * sideWidth, 0.0f, -1.0f,  0.0f,
+	         0.5f * sideWidth, -height, -0.5f * sideWidth, 0.0f, -1.0f,  0.0f,
+	         0.5f * sideWidth, -height,  0.5f * sideWidth, 0.0f, -1.0f,  0.0f,
+	         0.5f * sideWidth, -height,  0.5f * sideWidth, 0.0f, -1.0f,  0.0f,
+	        -0.5f * sideWidth, -height,  0.5f * sideWidth, 0.0f, -1.0f,  0.0f,
+	        -0.5f * sideWidth, -height, -0.5f * sideWidth, 0.0f, -1.0f,  0.0f,
 
-	        -0.5f * sideWidth,  height, -0.5f * sideWidth,  0.0f, 1.0f,
-	         0.5f * sideWidth,  height, -0.5f * sideWidth,  1.0f, 1.0f,
-	         0.5f * sideWidth,  height,  0.5f * sideWidth,  1.0f, 0.0f,
-	         0.5f * sideWidth,  height,  0.5f * sideWidth,  1.0f, 0.0f,
-	        -0.5f * sideWidth,  height,  0.5f * sideWidth,  0.0f, 0.0f,
-	        -0.5f * sideWidth,  height, -0.5f * sideWidth,  0.0f, 1.0f
+	        -0.5f * sideWidth,  height, -0.5f * sideWidth, 0.0f,  1.0f,  0.0f,
+	         0.5f * sideWidth,  height, -0.5f * sideWidth, 0.0f,  1.0f,  0.0f,
+	         0.5f * sideWidth,  height,  0.5f * sideWidth, 0.0f,  1.0f,  0.0f,
+	         0.5f * sideWidth,  height,  0.5f * sideWidth, 0.0f,  1.0f,  0.0f,
+	        -0.5f * sideWidth,  height,  0.5f * sideWidth, 0.0f,  1.0f,  0.0f,
+	        -0.5f * sideWidth,  height, -0.5f * sideWidth, 0.0f,  1.0f,  0.0f,
 	    };
 
 		vertices = new GLfloat[verticesNo * verticeCoordNo];
@@ -65,16 +65,25 @@ public:
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, this->GetArraySize(), this->GetVertices(), GL_STATIC_DRAW);
 
-		    // Position attribute
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *)0);
+		// Position attribute
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
 		glEnableVertexAttribArray(0);
-		    // TexCoord attribute
-		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *)(3*sizeof(GLfloat)));
+
+		// Normal attribute
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 		glEnableVertexAttribArray(1);
 
 		glBindVertexArray(0);
+	}
 
-	    glGenTextures(1, &texture);
+	~Platform() {
+		delete[] vertices;
+		glDeleteVertexArrays(1, &VAO);
+	    glDeleteBuffers(1, &VBO);
+	}
+
+	void CreateTexture() {
+		glGenTextures(1, &texture);
 	    glBindTexture(GL_TEXTURE_2D, texture); // All upcoming GL_TEXTURE_2D operations now have effect on our texture object
 	    // Set our texture parameters
 	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -89,12 +98,6 @@ public:
 	    glGenerateMipmap(GL_TEXTURE_2D);
 	    SOIL_free_image_data(image);
 	    glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
-	}
-
-	~Platform() {
-		delete[] vertices;
-		glDeleteVertexArrays(1, &VAO);
-	    glDeleteBuffers(1, &VBO);
 	}
 
 	GLfloat *GetVertices() {
@@ -121,10 +124,11 @@ public:
 		return texture;
 	}
 
+
 private:
 	const size_t verticesNo = 6*6;
-	const size_t verticeCoordNo = 5;
-	const GLfloat height = 0.01f;
+	const size_t verticeCoordNo = 6;
+	const GLfloat height = 1.01f;
 
 	GLuint VBO;
 	GLuint VAO;
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-// Std. Includes
-#include <string>
+#include <iostream>
+#include <cmath>
 
 // GLEW
 #define GLEW_STATIC
@@ -8,52 +8,54 @@
 // GLFW
 #include <GLFW/glfw3.h>
 
-// GL includes
-#include "Shader.hpp"
-#include "Camera.hpp"
-
-#include "Platform.hpp"
-#include "Trunk.hpp"
-#include "Gear.hpp"
-
-// GLM Mathemtics
+// Other Libs
+#include <SOIL/SOIL.h>
+// GLM Mathematics
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-// Other Libs
-#include <SOIL/SOIL.h>
+// Other includes
+#include "Shader.hpp"
+#include "Camera.hpp"
+#include "Platform.hpp"
 
-// Properties
-GLuint screenWidth = 1024, screenHeight = 768;
 
 // Function prototypes
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-void Do_Movement();
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void do_movement();
+
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
 
 // Camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-bool keys[1024];
-GLfloat lastX = 400, lastY = 300;
-bool firstMouse = true;
+Camera  camera(glm::vec3(0.0f, 0.0f, 3.0f));
+GLfloat lastX  =  WIDTH  / 2.0;
+GLfloat lastY  =  HEIGHT / 2.0;
+bool    keys[1024];
 
-GLfloat deltaTime = 0.0f;
-GLfloat lastFrame = 0.0f;
+// Light attributes
+glm::vec3 lightPos(1.2f, 1.0f, 1.0f);
 
-// The MAIN function, from here we start our application and run our Game loop
+// Deltatime
+GLfloat deltaTime = 0.0f;	// Time between current frame and last frame
+GLfloat lastFrame = 0.0f;  	// Time of last frame
+
+// The MAIN function, from here we start the application and run the game loop
 int main()
 {
     // Init GLFW
     glfwInit();
+    // Set all the required options for GLFW
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    glfwWindowHint(GLFW_SAMPLES, 4);
 
-    GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "LearnOpenGL", nullptr, nullptr); // Windowed
+    // Create a GLFWwindow object that we can use for GLFW's functions
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
     glfwMakeContextCurrent(window);
 
     // Set the required callback functions
@@ -61,119 +63,134 @@ int main()
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
 
-    // Options
+    // GLFW Options
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-    // Initialize GLEW to setup the OpenGL Function pointers
+    // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
     glewExperimental = GL_TRUE;
+    // Initialize GLEW to setup the OpenGL Function pointers
     glewInit();
 
     // Define the viewport dimensions
-    glViewport(0, 0, screenWidth, screenHeight);
+    glViewport(0, 0, WIDTH, HEIGHT);
 
-    // Setup some OpenGL options
+    // OpenGL options
     glEnable(GL_DEPTH_TEST);
 
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    // Setup and compile our shaders
-    Shader ourShader("shader.vs", "shader.frag");
+    // Build and compile our shader program
+    Shader lightingShader("lighting.vs", "lighting.frag");
+    Shader lampShader("lamp.vs", "lamp.frag");
 
-	//Platform platform(5.0f);
-	Gear gear(18);
-	Trunk trunk(120);
+	Platform platform(1.0f);
+	Platform lamp(1.0f);
 
     // Game loop
-    while(!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(window))
     {
-        // Set frame time
+        // Calculate deltatime of current frame
         GLfloat currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
-        // Check and call events
+        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
         glfwPollEvents();
-        Do_Movement();
+        do_movement();
 
         // Clear the colorbuffer
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // Bind Textures using texture units
-        // glActiveTexture(GL_TEXTURE0);
-        // glBindTexture(GL_TEXTURE_2D, platform.GetTexture());
-        // glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture"), 0);
+        // Use cooresponding shader when setting uniforms/drawing objects
+        lightingShader.Use();
+        GLint objectColorLoc = glGetUniformLocation(lightingShader.Program, "objectColor");
+        GLint lightColorLoc  = glGetUniformLocation(lightingShader.Program, "lightColor");
+        GLint lightPosLoc    = glGetUniformLocation(lightingShader.Program, "lightPos");
+        GLint viewPosLoc     = glGetUniformLocation(lightingShader.Program, "viewPos");
+        glUniform3f(objectColorLoc, 1.0f, 0.0f, 0.0f);
+        glUniform3f(lightColorLoc,  1.0f, 1.0f, 1.0f);
+        glUniform3f(lightPosLoc,    lightPos.x, lightPos.y, lightPos.z);
+        glUniform3f(viewPosLoc,     camera.Position.x, camera.Position.y, camera.Position.z);
 
-        ourShader.Use();
 
-        // Create camera transformation
+
+        // Create camera transformations
         glm::mat4 view;
         view = camera.GetViewMatrix();
-        glm::mat4 projection;
-        projection = glm::perspective(camera.Zoom, (float)screenWidth/(float)screenHeight, 0.1f, 1000.0f);
+        glm::mat4 projection = glm::perspective(camera.Zoom, (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
         // Get the uniform locations
-        GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
-        GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
-        GLint projLoc = glGetUniformLocation(ourShader.Program, "projection");
+        GLint modelLoc = glGetUniformLocation(lightingShader.Program, "model");
+        GLint viewLoc  = glGetUniformLocation(lightingShader.Program,  "view");
+        GLint projLoc  = glGetUniformLocation(lightingShader.Program,  "projection");
         // Pass the matrices to the shader
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-		glm::mat4 model;
-
-		glBindVertexArray(trunk.GetVAO());
-        model = glm::translate(model, glm::vec3(0.0f, -0.7f, -1.0f));
-        model = glm::rotate(model, 0.0f, glm::vec3(1.0f, 0.3f, 0.5f));
+        // Draw the container (using container's vertex attributes)
+        glBindVertexArray(platform.GetVAO());
+        glm::mat4 model;
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glDrawArrays(GL_TRIANGLES, 0, trunk.GetVerticesNo());
-
-		glBindVertexArray(gear.GetVAO());
-		model = glm::translate(model, glm::vec3(1.0f, 1.7f, -10.0f));
-        model = glm::rotate(model, 0.0f, glm::vec3(1.0f, 0.3f, 0.5f));
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glDrawArrays(GL_TRIANGLES, 0, gear.GetVerticesNo());
-
+        glDrawArrays(GL_TRIANGLES, 0, platform.GetVerticesNo());
         glBindVertexArray(0);
-        // Swap the buffers
+
+        // Also draw the lamp object, again binding the appropriate shader
+        lampShader.Use();
+        // Get location objects for the matrices on the lamp shader (these could be different on a different shader)
+        modelLoc = glGetUniformLocation(lampShader.Program, "model");
+        viewLoc  = glGetUniformLocation(lampShader.Program, "view");
+        projLoc  = glGetUniformLocation(lampShader.Program, "projection");
+        // Set matrices
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+        model = glm::mat4();
+        model = glm::translate(model, lightPos);
+        model = glm::scale(model, glm::vec3(0.2f)); // Make it a smaller cube
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        // Draw the light object (using light's vertex attributes)
+        glBindVertexArray(lamp.GetVAO());
+        glDrawArrays(GL_TRIANGLES, 0, lamp.GetVerticesNo());
+        glBindVertexArray(0);
+
+        // Swap the screen buffers
         glfwSwapBuffers(window);
     }
-    // Properly de-allocate all resources once they've outlived their purpose
+
+    // Terminate GLFW, clearing any resources allocated by GLFW.
     glfwTerminate();
     return 0;
-}
-
-// Moves/alters the camera positions based on user input
-void Do_Movement()
-{
-    // Camera controls
-    if(keys[GLFW_KEY_W])
-        camera.ProcessKeyboard(FORWARD, deltaTime);
-    if(keys[GLFW_KEY_S])
-        camera.ProcessKeyboard(BACKWARD, deltaTime);
-    if(keys[GLFW_KEY_A])
-        camera.ProcessKeyboard(LEFT, deltaTime);
-    if(keys[GLFW_KEY_D])
-        camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
 // Is called whenever a key is pressed/released via GLFW
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
-    //cout << key << endl;
-    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
     if (key >= 0 && key < 1024)
     {
-        if(action == GLFW_PRESS)
+        if (action == GLFW_PRESS)
             keys[key] = true;
-        else if(action == GLFW_RELEASE)
+        else if (action == GLFW_RELEASE)
             keys[key] = false;
     }
 }
 
+void do_movement()
+{
+    // Camera controls
+    if (keys[GLFW_KEY_W])
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (keys[GLFW_KEY_S])
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (keys[GLFW_KEY_A])
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (keys[GLFW_KEY_D])
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+bool firstMouse = true;
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
-    if(firstMouse)
+    if (firstMouse)
     {
         lastX = xpos;
         lastY = ypos;
@@ -188,7 +205,6 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 
     camera.ProcessMouseMovement(xoffset, yoffset);
 }
-
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
